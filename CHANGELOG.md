d0e7ca090d064c2c8fb428455d85e8f8f6989714
Michael Schroeder <schroeder@princeton.edu>
2023-11-07 17:15:14 -0500

    Setting up black precommit (squashed)
    Trying to get black precommit working
    
    Trying to get black precommit working 2
    
    Trying to get black precommit working 4
    
    Finishing up black precommit work
    
    Finishing up black precommit work2

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 11-7
+I think I got black precommit working
---
+++ black_test.py
@@ -0,0 +1,8 @@
+# making code that black will hopefully edit on precommit
+a = 41
+
+
+def func(x):
+    b = x**2
+
+    return b
