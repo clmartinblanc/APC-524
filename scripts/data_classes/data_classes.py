19918c9ba64fbf3eb45db07fa14e1ea70c8f2607
skoblens5 <skoblens5@gmail.com>
2023-11-14 13:11:39 -0500

    Added scripts/data_classes_data_classes.py

---
+++ scripts/data_classes/data_classes.py
@@ -0,0 +1,48 @@
+import abc
+from os import listdir
+from os.path import splitext
+
+
+class data(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self, dataPath, extension):
+        self.dataPath = dataPath
+        self.extension = extension
+
+    @abc.abstractmethod
+    def numberCases(self):
+        pass
+
+
+class videoData(data):
+    def __init__(self, dataPath, extension):
+        videoData.dataPath = dataPath
+        videoData.extension = extension
+        # Read all file names in path with designated extension
+        videoData.fileNames = [
+            splitext(f)[0] for f in listdir(self.dataPath) if f.endswith(self.extension)
+        ]
+        videoData.ignore = {}
+
+    def numberCases(self):
+        return len(self.fileNames)
+
+    def ignoreFile(self, names):
+        for file_name in names:
+            if file_name not in videoData.ignore:
+                videoData.ignore.append(file_name)
+
+    def removeFile(self, names):
+        for file_name in names:
+            if file_name in videoData.fileNames:
+                videoData.fileNames.remove(file_name)
+
+    def addFile(self, names):
+        for file_name in names:
+            if file_name not in videoData.fileNames:
+                videoData.fileNames.append(file_name)
+
+    def updateAll(self):
+        videoData.fileNames = [
+            splitext(f)[0] for f in listdir(self.dataPath) if f.endswith(self.extension)
+        ]
