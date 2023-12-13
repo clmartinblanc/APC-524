e4e4c39c150244fda556af76a40fa9e54e8950f0
skoblens5 <skoblens5@gmail.com>
2023-12-12 21:17:52 -0500

    Progressing in video demo

---
+++ scripts/data_classes/.ipynb_checkpoints/data_classes-checkpoint.py
@@ -0,0 +1,80 @@
+import abc
+from os import listdir
+from os.path import splitext
+
+
+class data(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self, data_path, extension):
+        self.data_path = data_path
+        self.extension = extension
+
+    @abc.abstractmethod
+    def number_cases(self):
+        pass
+
+
+class VideoData(data):
+    def __init__(self, data_path, extension):
+        VideoData.data_path = data_path
+        VideoData.extension = extension
+        # Read all file names in path with designated extension
+        VideoData.file_names = [
+            splitext(f)[0]
+            for f in listdir(self.data_path)
+            if f.endswith(self.extension)
+        ]
+        VideoData.ignore = {}
+
+    def number_cases(self):
+        return len(self.file_names)
+
+    def ignore_file(self, names):
+        for file_name in names:
+            if file_name not in VideoData.ignore:
+                VideoData.ignore.append(file_name)
+
+    def remove_file(self, names):
+        for file_name in names:
+            if file_name in VideoData.file_names:
+                VideoData.file_names.remove(file_name)
+
+    def add_file(self, names):
+        for file_name in names:
+            if file_name not in VideoData.file_names:
+                VideoData.file_names.append(file_name)
+
+    def update_all(self):
+        VideoData.file_names = [
+            splitext(f)[0]
+            for f in listdir(self.data_path)
+            if f.endswith(self.extension)
+        ]
+
+
+class TableData(data):
+    def __init__(self, data_path, extension):
+        TableData.data_path = data_path
+        TableData.extension = extension
+
+    def number_cases(self):
+        pass
+
+    # takes self, returns data in array form
+    def get_array(self):
+        opened = open(self.data_path)
+        readed = opened.read()
+
+        # make string table into array table
+        lines = readed.split("\n")
+        data = [line.split(",") for line in lines]
+
+        # convert string numbers to float
+        for i in range(1, len(data)):
+            data[i] = [float(x) for x in data[i]]
+
+        return data
+
+
+def test():
+    print("TEST FUNCTION RAN")
