3d173d4ee9e2d2338f4f77b6a54a92700a95f5ef
Michael Schroeder <schroeder@princeton.edu>
2023-12-14 11:41:34 -0500

    Modified TableData to work with video ouput, added tests to ensure correct video output
    Modified TableData to work with video ouput, added tests to ensure correct video output

--- scripts/data_classes/testing_data_classes.py
+++ scripts/data_classes/testing_data_classes.py
@@ -1,5 +1,5 @@
 """
-I put test_data.txt in git so everyone can have it.
+I put test_data.csv in git so everyone can have it.
 
 The code runs using this file stored in your personal computer.
 
@@ -9,13 +9,15 @@ Change the filepath to where it is stored.
 print("\n--------------\n")
 
 import numpy as np
-import data_classes as data_classes_py
+import data_classes as data_classes
 import pandas as pd
 
 
-test_instance = data_classes_py.TableData("scripts/data_classes/test_data.txt", ".txt")
+test_instance = data_classes.TableData("scripts/data_classes/test_data.csv", ".csv")
 my_array = test_instance.get_array()
-
 print(my_array)
-
 print(type(test_instance))
+
+csv_instance = data_classes.TableData("scripts/video_demo/output/Data_RunA.csv", ".csv")
+ar = csv_instance.get_array()
+print(ar)
